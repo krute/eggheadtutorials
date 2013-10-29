1e0bb7fb7d49c78473c40e0340a0653ffa10e60a
krute <jason@krute.net>
2013-10-28 18:49:02 -0700

    monday js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var app = angular.module("app", []);
+
+app.directive("zippy", function($templateCache) {
+	console.log($templateCache)
+	
+	return{
+		restrict: "E",
+		transclude: true,
+		scope:{
+			title: "@"
+		},
+		templateUrl: 'zippy.html',
+		link: function(scope) {
+			scope.isContentVisible = false;
+			scope.toggleContent = function() {
+				scope.isContentVisible = !scope.isContentVisible;
+			}
+		}
+	}
+})
